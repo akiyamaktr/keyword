d0c349b6f5620ac1016ff167dd825515ef7025cf
akiyamaktr <107348218+akiyamaktr@users.noreply.github.com>
2022-06-12 19:27:45 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import kw
 
 def main():
   with st.form("my_form"):
-    textarea_val=st.text_area("Texts",value="物を2枚の刃で挟んで切る道具。切符などに穴をあけたり切り込みを入れたりする道具。パンチ。カニ・エビなどの節足動物の脚の、物をはさむような形に発達した部分。これをもつ脚を鉗脚という。\n体長40～60センチのものが多く、一般に耳が長く、前肢は短く、後肢は長い。上唇は縦に裂け、上あごの門歯は二対ある。品種は多く、肉は食用、毛皮は襟巻きなどにし、医学実験用・愛玩用ともする。")
+    textarea_val=st.text_area("Texts",value="")
     submitted = st.form_submit_button("Submit")
   if submitted:
     texts=textarea_val.splitlines()
