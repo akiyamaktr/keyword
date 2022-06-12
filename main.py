65395ecf404fd14de17c21342043db339888db17
akiyamaktr <107348218+akiyamaktr@users.noreply.github.com>
2022-06-12 19:30:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 import streamlit as st
-import json
-from urllib import request
-import time
 import kw
 
-
 def main():
-  
+  st.title("Keyword")  
   with st.form("my_form"):
     textarea_val=st.text_area("Texts",value="")
     submitted = st.form_submit_button("Submit")
@@ -22,12 +18,7 @@ def main():
       lines.append(line)
     text_dst="\n".join(lines)
 
-
-
-      
     st.text_area("Result",value=text_dst)
-
-
 
 if __name__ == "__main__":
   main()
